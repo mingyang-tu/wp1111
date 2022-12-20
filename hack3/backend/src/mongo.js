c4e3c5a9f958317735b6eb8e638dd9bff959863b
涂銘洋 <81792743+mingyang-tu@users.noreply.github.com>
2022-12-20 10:46:39 +0800

    hack3

---
+++ hack3/backend/src/mongo.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+import { dataInit } from "./upload.js";
+
+import "dotenv-defaults/config.js";
+
+mongoose.set("strictQuery", true);
+
+async function connect() {
+  // TODO 1 Connect to your MongoDB and call dataInit()
+  if (!process.env.MONGO_URL) {
+    console.error("Missing MONGO_URL!!!");
+    process.exit(1);
+  }
+  mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: "hack3"
+  }).then(async res => {
+    dataInit();
+    console.log("MongoDB connection created");
+  })
+  mongoose.connection.on(
+    "error",
+    console.error.bind(console, "Connection error:")
+  );
+  // TODO 1 End
+}
+
+export default { connect };
